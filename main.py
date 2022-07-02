db47ce89dc26a402dc994c091fb1ca529833b2eb
miguel <miguel.munoz@udem.edu>
2022-07-02 19:59:19 -0400

    initial-commit

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+Author: Miguel Angel Muñoz Rizo
+First Created: July 2nd, 2022
+
+This python project extracts text from selected pdfs and puts them in an Excel file.
+"""
+
+
+def main():
+    print("main")
+
+
+if __name__ == '__main__':
+    main()
