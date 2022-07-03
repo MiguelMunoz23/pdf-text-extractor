df5bab4c536b3e820d527467228046a64ac2ba05
miguel <miguel.munoz@udem.edu>
2022-07-02 20:43:24 -0400

    prompt window to select path and get all files in directory

--- main.py
+++ main.py
@@ -4,10 +4,28 @@ First Created: July 2nd, 2022
 
 This python project extracts text from selected pdfs and puts them in an Excel file.
 """
+import os
+from tkinter import Tk, filedialog
 
 
 def main():
-    print("main")
+    """
+    TODO:
+    - Extract text from file example and see how it is visualized.
+    """
+    # pointing root to Tk() to use it as Tk() in program.
+    root = Tk()
+    # Hides small tkinter window.
+    root.withdraw()
+    # Opened windows will be active. Above all windows despite selection.
+    root.attributes('-topmost', True)
+    # Returns opened path as str
+    open_file = filedialog.askdirectory()
+    print("Path: ", open_file)
+
+    # Return the list of files and directories present in a specified directory path.
+    list_files = os.listdir(open_file)
+    print("List of files: ", list_files)
 
 
 if __name__ == '__main__':
